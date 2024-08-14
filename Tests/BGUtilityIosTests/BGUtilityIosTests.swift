8ed89164e6349df278e776dae63750b2128124c7
macOS Sonoma <basant.kumar22@gmail.com>
2024-08-14 15:47:29 +0530

    commit for adding tag

--- Tests/BGUtilityIosTests/BGUtilityIosTests.swift
+++ Tests/BGUtilityIosTests/BGUtilityIosTests.swift
@@ -2,11 +2,14 @@ import XCTest
 @testable import BGUtilityIos
 
 final class BGUtilityIosTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testValidIndex() throws {
+        let numbers = [1,2,3,4,5]
+        let num = numbers[safeIndex: 0]
+        XCTAssert(num == 1)
+    }
+    func testInValidIndex() throws {
+        let numbers = [1,2,3,4,5]
+        let num = numbers[safeIndex: 10]
+        XCTAssert(num == nil)
     }
 }
